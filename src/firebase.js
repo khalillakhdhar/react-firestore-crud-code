d201b2629d09a8e0ddb1ea155f7908a30ba70967
khalil <khalillakhdharatc@gmail.com>
2022-04-22 15:41:03 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 let config = {
-  apiKey: "xxx",
-  authDomain: "xxx",
-  databaseURL: "xxx",
-  projectId: "xxx",
-  storageBucket: "xxx",
-  messagingSenderId: "xxx",
-  appId: "xxx",
+  apiKey: "AIzaSyC0K0GQgZU_AxY0Ynl8dhlm8F6VQSPUEDM",
+  authDomain: "pointage-e68de.firebaseapp.com",
+  projectId: "pointage-e68de",
+  storageBucket: "pointage-e68de.appspot.com",
+  messagingSenderId: "21481574273",
+  appId: "1:21481574273:web:d2e21ed76d81fe9f346280",
+  measurementId: "G-4CC0QLNJMJ"
 };
 
 firebase.initializeApp(config);
